d9fe0ad953e862899edd91d95939c4797a874b13
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2025-06-26 16:36:13 -0400

    Save images

--- convert_frames.sh
+++ convert_frames.sh
@@ -1,7 +1,7 @@
 IFS=$'\n'
-search_dir="./input"
+search_dir="./imgs/input/tclahe"
 for entry in $(ls $search_dir)
 do
-    ./src/dehazeDCP input/"$entry" results/"$entry" -s 7 -p 0.1 -w 0.7 -r 30 -e 0.0001 -t 0.1 -u -x
+    ./src/dehazeDCP imgs/input/tclahe/"$entry" imgs/output/tclahe_udcp/"$entry" -s 7 -p 0.1 -w 0.95 -r 30 -e 0.0001 -t 0.1 -u
     echo $entry
 done
